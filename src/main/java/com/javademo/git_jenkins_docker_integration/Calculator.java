76d857672fa4adf520953e08b22729e8f6c5927e
sujeet127 <sujeet.kumar6921@gmail.com>
2022-06-27 14:36:53 +0530

    first commit

---
+++ src/main/java/com/javademo/git_jenkins_docker_integration/Calculator.java
@@ -0,0 +1,10 @@
+package com.javademo.git_jenkins_docker_integration;
+
+/**
+ * Simple calculator interface.
+ */
+public interface Calculator {
+    int add(int x, int y);
+    int multiply(int x, int y);
+    void save(int x);
+}
